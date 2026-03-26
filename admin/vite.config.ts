cc2b0b57e3154fc94d58a2936cc9ed64688a16ea
Agyei Michael Junior <agyeimchjunior@gmail.com>
2026-03-26 16:26:17 +0000

    corrected the base in the project

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: { port: 3001 }
+  server: { port: 3001 },
+  base: "/",
 })
